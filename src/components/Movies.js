768a29e0abe1475ef714b64c8d542940b6406527
Shreyash Shah <shahshreyash.14@gmail.com>
2022-10-01 02:08:34 +0530

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -32,6 +32,8 @@ import {
     MDBModalFooter,
     // MDBContainer
 } from 'mdb-react-ui-kit';
+import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroller';
 
 
 const Movies = () => {
@@ -45,6 +47,9 @@ const Movies = () => {
     const [data2, setData2] = useState([])
     const [data3, setData3] = useState([])
     const [data4, setData4] = useState([])
+    const [page, setPage] = useState(1)
+
+    const [totalPages, setTotalPages] = useState(1)
     // const [data5, setData5] = useState({})
     const [title, setTitle] = useState('')
     const [backdrop_path, setbackdrop_path] = useState(null)
@@ -54,63 +59,78 @@ const Movies = () => {
     const [production, setProduction] = useState([])
     const [tagline, setTagline] = useState('')
     const [release_date, setRelease_date] = useState('')
-    // const [id, setId] = useState(256040)
+    const [loading, setLoading] = useState(true)
+    const [loading2, setLoading2] = useState(true)
+
 
 
 
     let parsedresults = [];
-    // let parsedresults5 = [];
     const fetchData = async () => {
 
-        const url = "https://api.themoviedb.org/3/discover/movie?api_key=c85a7220743f2e910ce5418be14ce8b8&with_origin_country=IN&include_video=true&append_to_response=videos,images"
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=c85a7220743f2e910ce5418be14ce8b8&with_origin_country=IN&include_video=true&append_to_response=videos,images&page=${page}`
 
+        setLoading(true);
         let results = await fetch(url)
         parsedresults = await results.json()
         setData(parsedresults.results)
-
-        // console.log(parsedresults)
-
+        setTotalPages(parsedresults.total_pages)
+        setPage(parsedresults.page)
+        setLoading(false)
     }
 
-    // const fetchpopular = async () => {
+    useEffect(() => {
 
-    //     const url = "https://api.themoviedb.org/3/movie/popular?api_key=c85a7220743f2e910ce5418be14ce8b8&with_origin_country=IN&include_video=true&append_to_response=videos,images"
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-    //     let results = await fetch(url)
-    //     let parsedresults5 = await results.json()
-    //     setData5(parsedresults5.results)
+    const fetchMoreData = async () => {
+        // setPage(page + 1)
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=c85a7220743f2e910ce5418be14ce8b8&with_origin_country=IN&include_video=true&append_to_response=videos,images&page=${page + 1}`
 
-    //     console.log(parsedresults5)
-    // }
-
+        // setLoading(true);
+        let results = await fetch(url)
+        parsedresults = await results.json()
+        setLoading(false)
+        setData(data.concat(parsedresults.results))
+        setPage(parsedresults.page)
+    }
 
     const fetchItems = async (id) => {
 
 
         const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=c85a7220743f2e910ce5418be14ce8b8&include_video=true&append_to_response=videos,images`
-
+        setLoading2(true)
         let results = await fetch(url)
         let parsedresults2 = await results.json()
         setData2(parsedresults2.results)
+        setLoading2(false)
         // console.log(parsedresults2)
         // eslint-disable-next-lin
 
 
         const url2 = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=c85a7220743f2e910ce5418be14ce8b8&append_to_response=videos,images`
+        setLoading2(true)
         let cast = await fetch(url2)
         let parsedresults3 = await cast.json()
         setData3(parsedresults3.cast)
+        setLoading2(false)
         // console.log(parsedresults3)
 
         const url3 = `https://api.themoviedb.org/3/movie/${id}/images?api_key=c85a7220743f2e910ce5418be14ce8b8&append_to_response=videos,images`
+        setLoading2(true)
         let backdrops = await fetch(url3)
         let parsedresults4 = await backdrops.json()
         setData4(parsedresults4.backdrops)
+        setLoading2(false)
         // console.log(parsedresults4)
 
         const url4 = `https://api.themoviedb.org/3/movie/${id}?api_key=c85a7220743f2e910ce5418be14ce8b8&include_video=true&append_to_response=videos,images`
+        setLoading2(true)
         let results5 = await fetch(url4)
         let parsedresults5 = await results5.json()
+        setLoading2(false)
         // setData5(parsedresults5.results5)
         setTitle(parsedresults5.title)
         setbackdrop_path(parsedresults5.backdrop_path)
@@ -148,80 +168,81 @@ const Movies = () => {
     //     console.log(parsedresults3)
     // }
 
-    useEffect(() => {
 
-        fetchData()
-        // fetchVideos()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     const image_path = "https://image.tmdb.org/t/p/original";
     const video_url = "https://www.youtube.com/embed/"
 
     return (
         <div className='container my-4'>
+            {loading && <Spinner />}
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={fetchMoreData}
+                hasMore={totalPages > page}
+                loader={<Spinner />}
+            >
 
-            <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-                {data.map((element) => {
-                    return <MDBCol key={element.id}>
-                        {/* <Routes>
-                            <Route path="/moviesmore" element={<Moviesmore id={element.id} />} />
-                        </Routes> */}
-                        <MDBCard className='h-100 mb-3' alignment="start" background='light' shadow="1"  >
-                            <MDBRipple
-                                className='bg-image hover-overlay shadow-1-strong rounded'
-                                rippleTag='div'
-                                rippleColor='light'
-                            >
-                                <MDBCardImage
-                                    src={`${image_path}${element.poster_path}`}
-                                    // src={`${image_path}${element.backdrop_path}`}
-                                    alt='...'
-                                    position='top'
-                                    className='img-fluid shadow-4 '
-                                // style={{ maxHeight: '300px' }}
+                <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+                    {data.map((element) => {
+                        return <MDBCol key={element.id}>
 
-                                />
-                                <a href='#!'>
-                                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
-                                </a>
-                            </MDBRipple>
-                            {/* <MDBCardImage
-                                src={`${image_path}${element.poster_path}`}
-                                // src={`${image_path}${element.backdrop_path}`}
-                                alt='...'
-                                position='top'
-                                className='img-fluid shadow-4'
-                            /> */}
-                            <MDBCardBody>
-                                <MDBCardTitle className='text-center'>{element.title}</MDBCardTitle>
-                                <MDBCardTitle className='text-center' id='123'>{element.id}</MDBCardTitle>
-                                <MDBCardText >
-                                    {element.overview}
-                                </MDBCardText>
-                                <MDBCardText>
-                                    <i className="fa-solid fa-star">{element.vote_average}</i>
-                                </MDBCardText>
-                                <MDBCardText>
-                                    <small className='text-muted'>Release date: {element.release_date}</small>
-                                </MDBCardText>
-                            </MDBCardBody>
-                            <MDBCardFooter>
+                            <MDBCard className='h-100 mb-3' alignment="start" background='light' shadow="1"  >
+                                <MDBRipple
+                                    className='bg-image hover-overlay shadow-1-strong rounded'
+                                    rippleTag='div'
+                                    rippleColor='light'
+                                >
+                                    <MDBCardImage
+                                        src={`${image_path}${element.poster_path}`}
+                                        // src={`${image_path}${element.backdrop_path}`}
+                                        alt='...'
+                                        position='top'
+                                        className='img-fluid shadow-4 '
+                                        style={{ maxHeight: '400px' }}
 
-                                <MDBBtn onClick={toggleShow} >
-                                    <MDBBtn onClick={() => fetchItems(element.id)}>
-                                        LAUNCH DEMO MODAL
+                                    />
+                                    <a href='#!'>
+                                        <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+                                    </a>
+                                </MDBRipple>
+                                <MDBCardBody className='position'>
+                                    <MDBCardTitle className='text-center'>{element.title}</MDBCardTitle>
+                                    <MDBCardText>
+                                        <div style={{ marginBottom: "50px" }}>
+                                            {element.overview}
+                                        </div>
+                                    </MDBCardText>
+                                    <MDBCardText>
+                                        <div className='bottomleft2'>
+                                            {(element.vote_average !== 0)
+                                                ? <i className="fa-solid fa-star">{element.vote_average}</i>
+                                                : <i className="fa-solid fa-star">Not rated yet</i>}
+                                        </div>
+                                    </MDBCardText>
+                                    <MDBCardText>
+                                        <div className='text-muted bottomleft' >Release date: {element.release_date}</div>
+                                    </MDBCardText>
+
+                                </MDBCardBody>
+                                <MDBCardFooter>
+
+                                    <MDBBtn onClick={toggleShow} >
+                                        <MDBBtn onClick={() => fetchItems(element.id)}>
+                                            LAUNCH DEMO MODAL
+                                        </MDBBtn>
                                     </MDBBtn>
-                                </MDBBtn>
 
-                            </MDBCardFooter>
+                                </MDBCardFooter>
 
 
-                        </MDBCard>
-                    </MDBCol>
+                            </MDBCard>
+                        </MDBCol>
 
-                })}
-            </MDBRow>
+                    })}
+                </MDBRow>
+
+            </InfiniteScroll>
 
 
 
@@ -234,27 +255,25 @@ const Movies = () => {
                             <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
                         </MDBModalHeader>
                         <MDBModalBody>
-                            <div className="row">
+                            {loading2 && <Spinner />}
+                            {!loading2 && <div className="row">
                                 <div className="card mb-3">
                                     {(backdrop_path !== null)
                                         ? <img src={`${image_path}${backdrop_path}`} className="card-img-top rounded mx-auto d-block my-2" alt='' style={{ maxHeight: '700px', maxWidth: '700px' }} />
                                         : <img src={`${image_path}${poster_path}`} className="card-img-top rounded mx-auto d-block my-2" alt='' style={{ maxHeight: '500px', maxWidth: '500px' }} />}
                                     <div className="card-body">
-                                        <h5 className="card-title text-center
-                                        ">{`${title}`}</h5>
+                                        {(`${title}` !== '') && <h5 className="card-title text-center">{`${title}`}</h5>}
                                         {(`${tagline}` !== '') && <><b>Tagline:</b><span className="card-text">{` ${tagline}`}</span></>}
                                         <br />
-                                        <b>Overview:</b><span className="card-text">{`${overview}`}</span>
+                                        {(`${overview}` !== '') && <><b>Overview:</b><span className="card-text">{`${overview}`}</span></>}
                                         <br />
-                                        <b>Release Date:</b><span className="card-text">{`${release_date}`}</span>
+                                        {(`${release_date}` !== '') && <><b>Release Date:</b><span className="card-text">{`${release_date}`}</span></>}
                                         <br />
                                         <b>Genres:</b>
                                         {geners.map((element) => {
                                             return <span className="card-text">{` ${element.name} `}</span>
                                         })}
                                         <br />
-                                        <br />
-
                                         <b>Production House:</b>
                                         <div className="horizontal">
                                             {production.map((element) => {
@@ -274,14 +293,16 @@ const Movies = () => {
                                         <div className="horizontal">
                                             {data2.map((element) => {
                                                 return <div key={element.key} className="slide1">
-                                                    {/* <div className="ratio ratio-16x9"> */}
-                                                    <iframe
-                                                        src={`${video_url}${element.key}`}
-                                                        title="YouTube video"
-                                                        allowFullScreen
-                                                        style={{ maxHeight: '300px', maxWidth: '300px' }}
-                                                    ></iframe>
-                                                    {/* </div> */}
+                                                    {
+                                                        (element.results !== [])
+                                                            ? <><iframe
+                                                                src={`${video_url}${element.key}`}
+                                                                title="YouTube video"
+                                                                allowFullScreen
+                                                                style={{ maxHeight: '300px', maxWidth: '300px' }}
+                                                            ></iframe></>
+                                                            : <><p>Not Available</p></>
+                                                    }
                                                 </div>
                                             })}
                                         </div>
@@ -292,11 +313,12 @@ const Movies = () => {
                                         <div className="horizontal">
                                             {data3.map((element) => {
                                                 return <div key={element.id} className="card slide1 text-center" style={{ width: '15rem' }}>
-
-                                                    <img src={`${image_path}${element.profile_path}`} className="card-img-top my-1" alt={`${element.name}`} style={{ maxHeight: '150px', maxWidth: '150px' }} />
+                                                    <a href={`${image_path}${element.profile_path}`} target='-blank' rel="noopener noreferrer">
+                                                        <img src={`${image_path}${element.profile_path}`} className="card-img-top my-1" alt={`${element.name}`} style={{ maxHeight: '150px', maxWidth: '150px', minHeight: '150px' }} />
+                                                    </a>
                                                     <div className="card-body">
-                                                        <p className="card-text">Name<br /> {element.name}</p>
-                                                        <p className="card-text">Character<br /> {element.character}</p>
+                                                        <div className="card-text">Name<br /> {element.name}</div>
+                                                        <div className="card-text">Character<br /> {element.character}</div>
                                                     </div>
                                                 </div>
                                             })}
@@ -310,7 +332,7 @@ const Movies = () => {
                                                 return <div key={element.file_path} className="slide1">
                                                     <div className="slide bg-image hover-overlay">
                                                         <a href={`${image_path}${element.file_path}`} target='-blank' rel="noopener noreferrer">
-                                                            <img src={`${image_path}${element.file_path}`} alt='...' className='img-fluid' style={{ maxHeight: '200px', maxWidth: '200px' }} />
+                                                            <img src={`${image_path}${element.file_path}`} alt='...' className='img-fluid' style={{ maxHeight: '300px', maxWidth: '300px' }} />
                                                         </a>
                                                         {/* <a href='#!'>
                                                             <div className='mask overlay' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
@@ -323,11 +345,11 @@ const Movies = () => {
                                         </div>
 
 
-                                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+
                                     </div>
                                 </div>
 
-                            </div>
+                            </div>}
                         </MDBModalBody>
 
                         <MDBModalFooter>
---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+        <div>
+            <div class="text-center">
+                <div class="spinner-grow text-dark" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
